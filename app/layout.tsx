61e7ecbe9ae7317ae9d09048a90c1c73ee72fea5
biplobrana007 <mubiplobrana@gmail.com>
2026-08-03 23:05:06 +0600

    initialized the app

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+
+import "./globals.css";
+
+
+export const metadata: Metadata = {
+  title: "FixItNow",
+  description: "This is a home service provider app",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`h-full antialiased`}
+    >
+      <body className="min-h-full ">{children}</body>
+    </html>
+  );
+}
